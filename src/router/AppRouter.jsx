53e9e9a533b95990b974c617f8f117b0e63a8cc3
Mateja <m97maric@gmail.com>
2025-04-13 15:33:21 +0200

    Add placeholders for Login and Register page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,6 +5,8 @@ import MySwaps from '../pages/MySwaps';
 import Profile from '../pages/Profile';
 import NotFound from '../pages/NotFound';
 import Layout from '../components/layout/Layout';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const AppRouter = () => {
   return (
@@ -16,6 +18,8 @@ const AppRouter = () => {
           <Route path="/swaps" element={<MySwaps />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Layout>
     </BrowserRouter>
